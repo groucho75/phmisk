b13dfce871d4cacb311341ab49e868f1967527ea
groucho75 <groucho75@email.it>
2014-04-09 16:40:52 +0200

    up controllers

--- app/controllers/Base.php
+++ app/controllers/Base.php
@@ -2,9 +2,23 @@
 
 namespace App\Controllers;
 
+/**
+ * Base controller
+ *
+ * This file contains a base controller class you can use as parent
+ * for your custom controllers.
+ * You can write heritable methods and properties for all controllers
+ * that extend it.
+ *
+ * @package     Phmisk
+ * @subpackage  Controllers
+ * @author      Alessandro Massasso <alo@eventualo.net>
+ * @license     GPL
+ * @link        https://github.com/groucho75/phmisk
+ */
 class Base
 {
-	// You can write heritable methods and properties for all controllers
+	// 
 }
 
 
--- app/controllers/Demo.php
+++ app/controllers/Demo.php
@@ -2,8 +2,27 @@
 
 namespace App\Controllers;
 
+/**
+ * Demo Custom controller
+ *
+ * This file contains a sample custom controller class. 
+ * You can edit it or create other custom controllers in this folder:
+ * they will be autoloaded.
+ *
+ * @package     Phmisk
+ * @subpackage  Controllers
+ * @author      Alessandro Massasso <alo@eventualo.net>
+ * @license     GPL
+ * @link        https://github.com/groucho75/phmisk
+ */
 class Demo extends Base
-{	
+{
+    
+   /**
+    * Test medthod
+    *
+    * @param    obj     the template instance
+    */    	
 	function test($tpl) 
 	{
 		$data = array(
