e6ef7502d6ed6d3a09a7195249de5f5b6f545954
groucho75 <groucho75@email.it>
2014-04-22 12:45:53 +0200

    using Sparrow database layer

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -44,7 +44,8 @@ if ( DB_USER != '' && DB_NAME != '' )
     try {
     	$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
     	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$db = new \Voodoo\VoodOrm($pdo);
+    	$db = new \Sparrow();
+        $db->setDb($pdo);
     } catch( PDOException $e ) {
     	echo '<strong>Impossibile to connect to database: please check the parameters provided in /app/config.php.</strong><br />'. $e->getMessage();
     }
