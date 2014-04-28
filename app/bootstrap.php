4a3fed00bd11e2fd86350475cd097c4816208ffa
groucho75 <groucho75@email.it>
2014-04-28 14:50:30 +0200

    up pdo bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -44,6 +44,8 @@ if ( DB_USER != '' && DB_NAME != '' )
     try {
     	$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
     	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	$pdo->exec("SET NAMES 'utf8';");
+    	
     	$db = new \Sparrow();
         $db->setDb($pdo);
     } catch( PDOException $e ) {
